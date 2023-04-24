dfe27f439881659695127b7c6f8a6b52740fadc5
amal-prem <aldamal.prem@gmail.com>
2023-04-24 19:45:15 -0400

    render about page

--- main_app/views.py
+++ main_app/views.py
@@ -7,4 +7,4 @@ def home(request):
   return HttpResponse('<h1>Hello Doggy</h1>')
 
 def about(request):
-  return HttpResponse('<h1> About the Doggy Collector</h1>')
+  return render(request, 'about.html')
