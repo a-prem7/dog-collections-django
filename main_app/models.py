bcbd56604e7403262f545aa234645e9cc92c3d84
amal-prem <aldamal.prem@gmail.com>
2023-04-25 16:16:59 -0400

    added admin for feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.urls import reverse
 
+# A tuple of 2-tuples
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
+
+
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -11,6 +20,20 @@ class Dog(models.Model):
     # new code below
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
+
+
+class Feeding(models.Model):
+   date = models.DateField('feeding date')
+   meal = models.CharField(max_length=1, choices=MEALS,
+                           # set the default value for meal to be 'B'
+                           default=MEALS[0][0])
+
+   # Create a cat_id FK
+   dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+   def __str__(self):
+       # Nice method for obtaining the friendly value of a Field.choice
+       return f"{self.get_meal_display()} on {self.date}"
