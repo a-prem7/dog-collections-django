eeabf9d023e4da71541b3e629af336e2f2908233
amal-prem <aldamal.prem@gmail.com>
2023-04-24 20:23:46 -0400

    rendered dog list

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('dogs/', views.dogs_index, name='index'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+# Add the Cat class & list and view function below the imports
+class Dog:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Henry', 'doodle', 'foul little demon', 3),
+  Dog('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Dog('Raven', 'black tripod', '3 legged cat', 4)
+]
+
 # Create your views here.
 
 def home(request):
@@ -8,3 +23,8 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+
+# Add new view
+def dogs_index(request):
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
