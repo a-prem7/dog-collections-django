67165a21a5092f0f3e6e7e9009c77bb904c054d8
amal-prem <aldamal.prem@gmail.com>
2023-06-28 20:50:01 -0400

    changed main app name

--- dogcollector/settings.py
+++ dogcollector/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main_app',
+    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,6 +129,5 @@ LOGOUT_REDIRECT_URL = '/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#config django app for heroku
-import django_on_heroku
+# config django app for heroku
 django_on_heroku.settings(locals())
